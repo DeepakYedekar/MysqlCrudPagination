ebf431d25b0635938436152d82b10139e6496dfb
deepakYedekar <deepakyedekar28@gmail.com>
2023-05-22 23:43:52 +0530

    minorChange

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get("/Employee", find);
 
 router.post("/Employee/addContacts/:id", addContacts);
 
-router.get("/Employee/updateContacts/:id", updateEmployeeContacts);
+router.patch("/Employee/updateContacts/:id", updateEmployeeContacts);
 
 router.patch("/Employee/:id", update);
 
